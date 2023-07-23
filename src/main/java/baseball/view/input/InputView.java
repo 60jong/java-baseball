4148cb50ed331e546fa5ec56fe27e1e8bc0aeba4
KONKUK\s_rudwhd515 <rudwhd515@gmail.com>
2023-07-24 01:56:06 +0900

    feat : 사용자의 입력을 받을 때 사용되는 InputView 객체 생성

---
+++ src/main/java/baseball/view/input/InputView.java
@@ -0,0 +1,12 @@
+package baseball.view.input;
+
+public class InputView {
+
+    public static void printGameStartMessage() {
+        System.out.println("숫자 야구 게임을 시작합니다.");
+    }
+
+    public static void printBaseballNumbersAskingMessage() {
+        System.out.print("숫자를 입력해주세요 : ");
+    }
+}
