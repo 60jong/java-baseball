a6d11b7b8fb5bbbfc7b59ef006b60c87de4dd694
KONKUK\s_rudwhd515 <rudwhd515@gmail.com>
2023-07-24 01:55:36 +0900

    feat : 게임에 사용되는 상수를 관리

---
+++ src/main/java/baseball/constant/GameConstants.java
@@ -0,0 +1,8 @@
+package baseball.constant;
+
+public class GameConstants {
+
+    public static final int BASEBALL_NUMBER_START_NUMBER = 1;
+    public static final int BASEBALL_NUMBER_END_NUMBER = 9;
+    public static final int BASEBALL_NUMBER_COUNT = 3;
+}
